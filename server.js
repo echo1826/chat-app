053892d316a84bf54beae918b5db9abacac93207
Ethan <ethan.cho1826@gmail.com>
2023-07-26 19:32:54 -0500

    changed folder paths for deployment

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(express.json());
 app.use(route);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../build")));
 }
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 const httpServer = http.createServer(app);
